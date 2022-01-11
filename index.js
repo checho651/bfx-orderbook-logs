ee1d453a53272dc2e7d2fbaa1454fd3c1b1deb07
checho651 <maquieiraezequiel@gmail.com>
2022-01-11 09:04:03 -0300

    Entry point.

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict'
+
+// Local dependencies.
+const {
+  pair,
+  precision,
+  obLength,
+  logArrayLength
+} = require('./config/input-parameters.json')
+
+// Third party dependencies.
+const bfxapi = require('bitfinex-api-node')
+
+let obArrToLog = []
+
+function logObArr (ob) {
+  obArrToLog.push({
+    mts: Date.now(),
+    bids: ob.bids,
+    asks: ob.asks
+  })
+  if (obArrToLog.length > logArrayLength) {
+    console.log('OB:', JSON.stringify(obArrToLog))
+    obArrToLog = []
+  }
+}
+
+async function main () {
+  const ws = new bfxapi.WSv2({ transform: true, manageOrderBooks: true })
+
+  await ws.open()
+  console.log(Date.now(), 'OPEN WS CONNECTION.')
+
+  await ws.subscribeOrderBook(pair, precision, obLength)
+  console.log(Date.now(), 'SUBSCRIBED TO OB.')
+
+  ws.on('error', async (e) => {
+    console.error(Date.now(), `WSv2 error: ${e.message || e}`)
+    if (ws.isOpen()) await ws.close()
+  })
+
+  ws.on('close', () => {
+    console.log(Date.now(), 'WS CLOSED')
+    setTimeout(() => main(), 5000)
+  })
+
+  ws.onOrderBook({ symbol: pair }, async (ob) => {
+    if (!ob) return false
+    logObArr(ob)
+  })
+}
+
+if (require.main === module) main()
